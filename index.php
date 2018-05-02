d54940038eaad225f576629292e59568d7517571
mgashini95 <mganisansala@gmail.com>
2018-05-02 13:29:08 +0530

    #3 Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Home Page</title>
+    </head>
+    <body>
+
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About Us</a></li>
+            <li><a href="">Services</a></li>
+            <li><a href="">Gallery</a></li>
+            <li><a href="">Bookings</a></li>
+            <li><a href="">Contact US</a></li>
+        </ul>
+        <div class="slide">
+            
+        </div>
+
+        <h1>Home Page</h1>
+    </body>
+</html>
